3eb9b8a5e40bcb72ae4c39de5b32668045efdc7e
Brandon Carlson <bcarlso@leantechniques.com>
2018-11-08 15:56:16 -0500

    Allow PDFs to be targets of links.

--- lib/cloudfront_link_checker.rb
+++ lib/cloudfront_link_checker.rb
@@ -1,5 +1,32 @@
 class CloudfrontLinkChecker
-  def self.scan_files
+  def self.scan_files(directory_to_scan)
+    Dir.chdir(directory_to_scan) do
+      system('pwd')
+      htmlfiles = File.join("**", "*.html")
+      files = Dir.glob(htmlfiles)
+      bad_files = {}
+      files.each do |file|
+        bad_links = []
+        results = File.readlines(file, :encoding => "UTF-8").grep /href="(.*)"/i
+        results.each do |line|
+          links = line.match(/href=(["'])(.*?)\1/i)[2]
+          exclude_start_with_list = ["https:", "#", "mailto:", "tel:"]
+          exclude_endings = [".ico", ".png", ".css", "/#contact-us", ".pdf"]
+          if !links.start_with?(*exclude_start_with_list) && !links.end_with?(*exclude_endings)
+            if !links.start_with?("/") || !links.end_with?("/")
+              bad_links.push(links)
+            end
+          end
+        end
+        # Add the bad files to a map keyed of filename
+        if !bad_links.empty?
+          bad_files[file] = bad_links
+        end
+      end
 
+      if !bad_files.empty?
+        raise "Bad links discovered. Links need to start and end with forward-slashs to be served by CloudFront: #{bad_files}"
+      end
+    end
   end
 end
--- spec/cloudfront_link_checker_spec.rb
+++ spec/cloudfront_link_checker_spec.rb
@@ -1,9 +1,13 @@
 require 'cloudfront_link_checker'
 
 RSpec.describe CloudfrontLinkChecker do
-  context "does RSpec run properly" do
-    it "does!" do
-      expect(1).to eq(1)
+  context "inspects hrefs for bad Cloudfront links" do
+    it "allow pdf files" do
+      allow(Dir).to receive(:chdir) { |&block| block.call() }
+      allow(Dir).to receive(:glob).and_return(["somefile.html"])
+      allow(File).to receive(:readlines).and_return(['<a href="/some.pdf">Link to PDF</a>'])
+
+      expect(CloudfrontLinkChecker.scan_files('app/_site')).to be_nil
     end
   end
 end
